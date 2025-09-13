1ef902f59a995fc4e10af782b94ca45ba09a7f70
Seemant Tripathi <tripathiseemant766@gmail.com>
2025-09-13 09:38:23 +0530

    init: task priority redis (no-priority)

--- djangoprojcelery/celeryworker/tasks.py
+++ djangoprojcelery/celeryworker/tasks.py
@@ -1,9 +1,22 @@
 from celery import shared_task
+import time
 
 @shared_task
-def task1():
+def tp1(queue='celery'):
+    time.sleep(3)
     return True
 
 @shared_task
-def task2():
+def tp2(queue='celery:1'):
+    time.sleep(3)
+    return True
+
+@shared_task
+def tp3(queue='celery:2'):
+    time.sleep(3)
+    return True
+
+@shared_task
+def tp4(queue='celery:3'):
+    time.sleep(3)
     return True
--- djangoprojcelery/djangoprojcelery/celery.py
+++ djangoprojcelery/djangoprojcelery/celery.py
@@ -5,13 +5,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoprojcelery.settings')
 
 app = Celery('djangoprojcelery')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.task_routes = {
-    'celeryworker.tasks.*': {'queue': 'queue1'},
-    'celeryworker.tasks.task2': {'queue': 'queue2'},
+# app.conf.task_routes = {
+#     'celeryworker.tasks.*': {'queue': 'queue1'},
+#     'celeryworker.tasks.task2': {'queue': 'queue2'},
+# }
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'queue_order_strategy': 'priority',
+    'separator': ':',
 }
 # Auto-discover tasks from all installed Django apps
 app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
