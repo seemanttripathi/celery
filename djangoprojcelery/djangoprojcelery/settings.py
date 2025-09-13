21a36ed166d726885fcb71ffd2da8a9268f02187
Seemant Tripathi <tripathiseemant766@gmail.com>
2025-09-13 06:25:29 +0530

    init: setting up celery-worker and result-backend

--- djangoprojcelery/djangoprojcelery/settings.py
+++ djangoprojcelery/djangoprojcelery/settings.py
@@ -105,3 +105,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', "redis://redis:6379/0")
